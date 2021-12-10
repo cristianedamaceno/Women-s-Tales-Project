2552545168b2d2ff03a7dd433cddd08a0519cf98
Cristiane Damaceno <cristinecarmozini@gmail.com>
2021-12-10 11:26:16 -0300

    Criando Postagem

--- backend/src/main/java/com/example/womensTales/model/Tema.java
+++ backend/src/main/java/com/example/womensTales/model/Tema.java
@@ -1,13 +1,22 @@
 package com.example.womensTales.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+
 
 @Entity
 @Table(name = "tb_tema")
@@ -26,6 +35,18 @@ public class Tema {
 	
 	@Size(min = 2, max = 100, message = "O ATRIBUTO PALAVRA-CHAVE DEVE TER NO MINIMO 1 E NO MAXIMO 100 CARACTERES!")
 	private String palavraChave;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 	public long getId() {
 		return id;
--- backend/src/main/java/com/example/womensTales/repository/PostagemRepository.java
+++ backend/src/main/java/com/example/womensTales/repository/PostagemRepository.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.womensTales.model.Tema;
+import com.example.womensTales.model.Postagem;
+
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
-	public List <Postagem> findAllByTipoContainingIgnoreCase(String tipo);
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 	
-
+	public List <Postagem> findAllByTemaContainingIgnoreCase(String tema);
+	
+	public List <Postagem> findAllByTituloAndTemaContainingIgnoreCase(String titulo, String tema);
+	
+	//possibilidade de utilizar o Or e economizar uma linha de método.
+	
+	
 }
