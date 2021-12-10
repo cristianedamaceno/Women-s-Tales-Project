a1ef3de293ba71dd8793eba76ea850908b35f831
unknown <john-nunes@outlook.com>
2021-12-10 10:39:54 -0300

    Atualização do DER

---
+++ backend/src/main/java/com/example/womensTales/controller/PostagemController.java
@@ -0,0 +1,22 @@
+package com.example.womensTales.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity <List <Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository)
+	}
+
+}
