f1d5a8bbd4088bad3959915be6e20a2a1126d449
IaraLima <iarasantos.cabluelo@gmail.com>
2022-02-01 09:33:58 -0300

    Tarefas a mais 1, implmentação

--- front-end/wt-project/src/pages/contato/Contato.tsx
+++ front-end/wt-project/src/pages/contato/Contato.tsx
@@ -22,12 +22,16 @@ function Contato() {
                     <Box paddingX={10}>
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center">Contato</Typography>
-                            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth {...register("nome", { required: true })} />
-                            {errors.nome?.type === 'required' && "O Nome é um atributo obrigatório!"}
-
-                            <TextField id="nome" label="Email" variant="outlined" margin="normal" fullWidth {...register("email", { required: true })} />
-                            {errors.email?.type === 'required' && "O Nome é um atributo obrigatório!"}
-
+                            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth {...register("nome", { required: true, maxLength: 30, minLength: 3 })} />
+                            {errors.nome?.type === 'required' && <span className="msgError"> "O Nome é um atributo obrigatório!"</span>}
+                            {errors.nome && errors.nome.type === "maxLength" && <span className="msgError">O tamanho para o nome foi excedido! Use no máximo 30 caracteres.</span> }
+                            {errors.nome && errors.nome.type === "minLength" && <span className="msgError">O tamanho para o nome está curto! Use mais de 3 caracteres.</span> }
+                            
+                            <TextField id="nome" label="Email" variant="outlined" margin="normal" fullWidth {...register("email", { required: true, maxLength: 100, minLength: 10, pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Endereço de email inválido!" } })} />
+                            {errors.email?.type === 'required' && <span className="msgError">O email é um atributo obrigatório!</span>}
+                            {errors.email && errors.email.type === "maxLength" && <span className="msgError">O tamanho para o email foi excedido! Use no máximo 100 caracteres.</span> }
+                            {errors.email && errors.email.type === "minLength" && <span className="msgError">O tamanho para o email está curto! Use mais de 10 caracteres.</span> }
+                            
                             <TextField
                                 id="outlined-multiline-flexible"
                                 variant="outlined"
@@ -36,11 +40,12 @@ function Contato() {
                                 multiline
                                 fullWidth
                                 margin="normal"
-                                {...register("mensagem", { required: true })}
+                                {...register("mensagem", { required: true, maxLength: 1500, minLength: 3 } )}
                             />
-
-                            {errors.email?.type === 'required' && "O Nome é um atributo obrigatório!"}
-
+                             {errors.mensagem?.type === 'required' && <span className="msgError">A mensagem é um atributo obrigatório!</span>}
+                            {errors.mensagem && errors.mensagem.type === "maxLength" && <span className="msgError">O tamanho para a mensagem foi excedido! Use no máximo 1500 caracteres.</span> }
+                            {errors.mensagem && errors.mensagem.type === "minLength" && <span className="msgError">O tamanho para a mensagem está curto! Use mais de 3 caracteres.</span> }
+                           
                             <Box marginTop={2} textAlign="center">
                                 <Link to="/home" className="text-decorator-none">
                                     <Button variant="contained" color="secondary" className="bgColorUser">
