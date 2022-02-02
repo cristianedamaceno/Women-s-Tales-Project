906090f3d7baffd53633fda49dc84da638d8aadd
Erick Costa <erick.costa362@gmail.com>
2022-02-02 12:06:50 -0300

    Adicionando função do Email

--- front-end/wt-project/src/pages/contato/Contato.tsx
+++ front-end/wt-project/src/pages/contato/Contato.tsx
@@ -3,25 +3,32 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import './Contato.css';
 import { Grid, Box, Typography, TextField, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import emailjs from "emailjs-com";
 
 type Inputs = {
     nome: string,
     email: string,
     mensagem: string
 
-  };
+};
 
-  function Contato() {
+function Contato() {
+    function sendEmail(e: any): void {
+        e.preventDefault();
+        emailjs.sendForm('GmailMessageWT', 'WomensTemplate', e.target, 'user_PaWt6qBcoiQoSKWf7JxZA').then((result) => { alert("Mensagem enviada com sucesso."); }, (error) => { alert(error.message) });
+        e.target.reset()
+    }
+
     const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid item xs={6} className="bgImageCadastro"></Grid>
+                <Grid item xs={6} className="bgImageContato"></Grid>
                 <Grid item xs={6} alignItems="center">
                     <Box paddingX={10}>
-                        <form onSubmit={handleSubmit(onSubmit)}>
+                        <form onSubmit={sendEmail}>
                             <Typography variant="h4" gutterBottom color="textPrimary" component="h3" align="center">Contato</Typography>
                             <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth {...register("nome", { required: true, maxLength: 30, minLength: 3 })} />
                             {errors.nome?.type === 'required' && <span className="msgError"> "O Nome é um atributo obrigatório!"</span>}
@@ -43,10 +50,10 @@ type Inputs = {
                                 margin="normal"
                                 {...register("mensagem", { required: true, maxLength: 1500, minLength: 3 } )}
                             />
-                             {errors.mensagem?.type === 'required' && <span className="msgError">A mensagem é um atributo obrigatório!</span>}
+                            {errors.mensagem?.type === 'required' && <span className="msgError">A mensagem é um atributo obrigatório!</span>}
                             {errors.mensagem && errors.mensagem.type === "maxLength" && <span className="msgError">O tamanho para a mensagem foi excedido! Use no máximo 1500 caracteres.</span> }
                             {errors.mensagem && errors.mensagem.type === "minLength" && <span className="msgError">O tamanho para a mensagem está curto! Use mais de 3 caracteres.</span> }
-                           
+
                             <Box marginTop={2} textAlign="center">
                                 <Link to="/home" className="text-decorator-none">
                                     <Button variant="contained" color="secondary" className="bgColorUser">
